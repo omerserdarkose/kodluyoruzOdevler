33e5df8ee9e1abbee74fe311726b08ed180c9311
omerserdarkose <omerserdarkose@gmail.com>
2021-09-09 00:15:38 +0300

    asian kitchen projesi tamamlandi

--- js/script.js
+++ js/script.js
@@ -1,128 +1,142 @@
-const newTaskText = document.querySelector("#newTaskName");
-const addBtn = document.querySelector("form");
-const editBtn = document.querySelector("#editBtn");
-const delBtn = document.querySelector("#delBtn");
-const chkBoxAll = document.querySelector("#customCheck1");
-const taskList = document.querySelector("#taskList");
-var click = false;
+// Id, title, category, price, img ve desc
+const menu = [
+    {
+        id: 0,
+        title: "LOREM",
+        category: "A",
+        price: "8.99",
+        img: "https://picsum.photos/id/88/200/200",
+        desc: "Lorem ipsum dolor sit amet.",
+    },
+    {
+        id: 1,
+        title: "IPSUM",
+        category: "B",
+        price: "5.99",
+        img: "https://picsum.photos/id/1/200/200",
+        desc: "Lorem ipsum dolor sit amet.",
+    },
+    {
+        id: 2,
+        title: "lorem",
+        category: "C",
+        price: "6.9",
+        img: "https://picsum.photos/id/22/200/200",
+        desc: "Lorem ipsum dolor sit amet.",
+    },
+    {
+        id: 3,
+        title: "lorem",
+        category: "B",
+        price: "89.99",
+        img: "https://picsum.photos/id/38/200/200",
+        desc: "Lorem ipsum dolor sit amet.",
+    },
+    {
+        id: 4,
+        title: "lorem",
+        category: "A",
+        price: "69.99",
+        img: "https://picsum.photos/id/14/200/200",
+        desc: "Lorem ipsum dolor sit amet.",
+    },
+    {
+        id: 5,
+        title: "lorem",
+        category: "D",
+        price: "49.99",
+        img: "https://picsum.photos/id/5/200/200",
+        desc: "Lorem ipsum dolor sit amet.",
+    },
+    {
+        id: 6,
+        title: "lorem",
+        category: "A",
+        price: "19.99",
+        img: "https://picsum.photos/id/67/200/200",
+        desc: "Lorem ipsum dolor sit amet.",
+    },
+    {
+        id: 7,
+        title: "lorem",
+        category: "D",
+        price: "29.99",
+        img: "https://picsum.photos/id/7/200/200",
+        desc: "Lorem ipsum dolor sit amet.",
+    },
+    {
+        id: 8,
+        title: "lorem",
+        category: "C",
+        price: "39.99",
+        img: "https://picsum.photos/id/81/200/200",
+        desc: "Lorem ipsum dolor sit amet.",
+    },
+];
 
-const checkboxInnerHtml =
-    '<input type="checkbox" aria-label="Checkbox for following text input" class="mr-3"><button type="button" class="close" aria-label="Close"><span aria-hidden="true"><i class="fas fa-times"></i></span></button>';
+const btnContainer = document.querySelector(".btn-container");
+const section = document.querySelector(".section-center");
+const btn = document.querySelectorAll(".btn-item");
 
-eventListeners();
-function eventListeners() {
-    addBtn.addEventListener("submit", addNewItem);
-    editBtn.addEventListener("click", editing);
-    delBtn.addEventListener("click", removing);
-    chkBoxAll.addEventListener("click", checkAll);
-    taskList.addEventListener("click", doneOrDelete);
-}
-
-//yeni gorev ekleme fonksiyonu
-function addNewItem(e) {
-    e.preventDefault();
-    if (newTaskText.value != "") {
-        //icerik bos degilse ekleme islemini gercelestiriyor
-        let liDOM = document.createElement("li");
-        liDOM.className = 'list-group-item';
-        liDOM.innerText = newTaskText.value;
-        taskList.appendChild(liDOM);
-        newTaskText.value = "";
-    } else {
-        alert("Please type your task correctly!");
-    }
-}
-
-function editing(e) {
-    e.preventDefault();
-    let liChildren = taskList.children;
-    if (!click) {
-        //butona daha once basilip basilmadigi kontrol ediliyor
-        //her gorev elemanin basina checkbox sonuna X isareti konuluyor
-        for (let i = 0; i < liChildren.length; i++) {
-            let text = liChildren[i].innerText;
-            liChildren[i].innerHTML = checkboxInnerHtml;
-            liChildren[i].appendChild(document.createTextNode(text));
+//tum menu listesi geziliyor reduce fonksiyonu ile categori isimleri toplaniyor, reduce fonk. baslangic degeri olarak da 'All' verilip categori olarak listeye dahil ediliyor
+const categoryNames = menu.reduce(
+    (catgList, item) => {
+        if (!catgList.includes(item.category)) {
+            catgList.push(item.category.toUpperCase());
         }
-        click = true;
-        //edit islemi aktif edildiginde islem bitmeden yeni kayit girilmemesi icin giris bolumleri disable duruma aliniyor
-        document.getElementById("addBtn").disabled = true;
-        newTaskText.placeholder = "Please Finish Editing";
-        newTaskText.value = "Please Finish Editing";
-        newTaskText.disabled = true;
-    } else {
-        for (let i = 0; i < liChildren.length; i++) {
-            let text = liChildren[i].textContent;
-            liChildren[i].innerHTML = "";
-            liChildren[i].classList.remove('del');//menu kapanirken silme editleri iptal ediliyor
-            liChildren[i].appendChild(document.createTextNode(text));
-        }
-        click = false;
-        chkBoxAll.checked = false;//menu kapanirken tumunu sec isaretli ise check ozelligi kaldiriliyor
-        document.getElementById("addBtn").disabled = false;
-        newTaskText.disabled = false;
-        newTaskText.value = "";
-        newTaskText.placeholder = "Type a New Task";
-    }
-}
+        return catgList;
+    },
+    ["All"]
+);
 
-//silme icin gerekli checkbox isaretlemesi veya bittigine dair uzerine tiklanma sonrasi gerekli classlar ekleniyor
-function doneOrDelete(e) {
-    e.stopPropagation();
-    console.log(e.target);
-    if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
-    } else if (e.target.tagName === "INPUT") {
-        //edit menusundeki checkbox
-        if (!e.target.checked) {//check kaldirilirsa
-            chkBoxAll.checked = false;
-            e.target.parentElement.classList.remove('del');
-        }
-        else {
-            e.target.parentElement.classList.add('del');
-        }
+//her kategori ismine ait buton elementleri olusturlup html icine ekleniyor
+const btnHTML = categoryNames.map((item) => {
+        return `<button type="button" class="btn btn-outline-secondary btn-item">${item}</button>`;
+    }).join("");
 
-    }
-    else if (e.target.tagName === "I") {
-        if (confirm('Are you sure you want to delete the selected items?')) {
-            e.target.parentElement.parentElement.parentElement.remove();
-        }
-        
+btnContainer.innerHTML = btnHTML;
 
-    }
-}
-//silme islemi icin ul altindaki tum li lere ulasilip, her birinin classlari kontrol ediliyor hitaminda del classini icerenler siliniyor
-function removing() {
-    if (confirm('Are you sure you want to delete the selected items?')) {
-        if(chkBoxAll.checked){                                                //eger tumunu sec aktifse
-            taskList.querySelectorAll("li").forEach(item => {item.remove()}); //tum li'ler siliniyor
-            editBtn.click();                                                  //edit menusu kapatiliyor
-        }else {                                                               //tumu secili degilse
-            taskList.querySelectorAll("li").forEach(item => {
-                if (item.className.includes("del")) {                         //secilmis olanlar
-                    item.remove();                                            //siliniyor  
-                }
-            });
-        }
-        chkBoxAll.checked = false;                          //silme islemi sonrasi tumunu sec butonu inaktif yapiliyor
-    }
-}
-//select all ve li'lerin icinde ki checkboxlar birbirinden bagimsiz. eger tumunu sec kismina tiklandiginda zaten secili olanlar varsa onlarin dutumunda degisiklik olmayacak. tersi durumda da tumunu sec kaldirilirken zaten secili olmayanlar varsa onlarin durumu degismeyecek
-function checkAll() {
-    if (chkBoxAll.checked) {    //tumunu sec'e tiklandigi zaman
-        taskList.querySelectorAll("li").forEach(item => {       //liste icindeki tum satirlarda
-            if (item.querySelector("input").checked == false) { //secili olmayanlar varsa
-                item.querySelector("input").click();            //secili hale getir
+
+//butonlari tasiyan div'e ulasilip her bir buton icin iceriginde yazan kategori ismine gore menu listesi filtreleniyor. ve liste ekranda gosterilmesi icin menuShow fonksiyonuna gonderiliyor
+btnContainer.childNodes.forEach((value) => {
+    if (value.nodeType == 1) {
+        value.addEventListener("click", () => {
+            if (value.textContent == "All") {
+                menuShow(menu);
+            } else {
+                menuShow(
+                    menu.filter((item) => {
+                        if (item.category == value.textContent) {
+                            return item;
+                        }
+                    })
+                );
             }
-        }
-        );
+        });
     }
-    else {                                                      //tumunu sec kaldirildiginda
-        taskList.querySelectorAll("li").forEach(item => {       //liste icindeki tum satirlarda
-            if (item.querySelector("input").checked == true) {  //secili olanlar varsa
-                item.querySelector("input").click();            //secimden cikar
-            }
-        }
-        );
-    }
+});
+
+function menuShow(catgName) {
+    const catgHTML = catgName
+        .map((item) => {
+            return `<div class="menu-items col-lg-6 col-sm-12">
+            <img
+              src=${item.img}
+              alt=${item.title}
+              class="photo"
+            />
+            <div class="menu-info">
+              <div class="menu-title">
+                <h4>${item.title}</h4>
+                <h4 class="price">${item.price}</h4>
+              </div>
+              <div class="menu-text">
+                ${item.desc}
+              </div>
+            </div>
+          </div>`;
+        })
+        .join("");
+    section.innerHTML = catgHTML.toString();
 }
+menuShow(menu);
